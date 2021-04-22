545f61918e31ee0de801ec07a92358ad4fa13159
Gustavo Simoes <gustavo@Air-de-Gustavo.box>
2021-04-22 02:18:42 -0300

    Header criado

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,13 +5,16 @@ export default class MyDocument extends Document{
         return(
             <Html>
                 <Head>
-                    
+                    {/* "preconnect" significa que o next deve tentar se conectar com o google fonts o quanto antes */}
                     <link rel="preconnect" href="https://fonts.gstatic.com"/>
                     <link href="https://fonts.googleapis.com/css2?family=Inter&family=Lexend:wght@500;600&display=swap" rel="stylesheet"/>
                 </Head>
                 <body>
-
+                    
+                    {/* A aplicação é rodada dentro do Main  */}
                     <Main/>
+
+                    {/* Scripts que o next precisa implementar */}
                     <NextScript/>
                 </body>
             </Html>
